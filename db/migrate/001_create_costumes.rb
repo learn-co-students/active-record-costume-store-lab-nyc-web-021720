3d797c448eb74a46794b2449fbdd1e5ed6180577
devmullets <devmullets@gmail.com>
2020-03-01 17:17:30 -0500

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,5 +1,16 @@
 # Create a class and inherit from ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[5.2]
+    def change 
+        create_table :costumes do |table_costumes|
+            table_costumes.string :name
+            table_costumes.integer :price
+            table_costumes.string :size
+            table_costumes.string :image_url
+            table_costumes.timestamps null: false
+        end 
+    end 
 
+end 
 # By convention, the class name should match the part of the
 # file name after the number, so in this case:
 # 002_create_costumes.rb becomes class CreateCostumes
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,19 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+
+    def change
+        create_table :haunted_houses do |houses|
+            houses.string :name
+            houses.string :location
+            houses.string :theme
+            houses.integer :price
+            houses.boolean :family_friendly
+            houses.datetime :opening_date
+            houses.datetime :closing_date
+            houses.string :description
+
+            houses.timestamps null: false
+        end 
+    end 
+
+end 
